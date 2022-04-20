f69721ee388e7bb0dce0cd88de99bc5e996bfaf2
Alexander Muravyev <aleksander.muravyev@qvantel.com>
2022-04-20 19:43:48 +0300

    fix: cassacdra backward compatibililty

--- modules/340-vault-configuration/hooks/acl-policy-hook.py
+++ modules/340-vault-configuration/hooks/acl-policy-hook.py
@@ -21,19 +21,21 @@ kubernetes:
   queue: AclPolicyQueue
 """)
     case EventHook(eventName, context):
-        name = context['object']['metadata']['name']
-        policy_name = context.get('object',{}).get('spec',{}).get('policy-name')
+        for val in context:
+            name = val['object']['metadata']['name']
+            eventName = val['watchEvent'] 
+            policy_name = val.get('object',{}).get('spec',{}).get('policy-name')
 
-        secret = v1.read_namespaced_secret("vault-keys", "platform").data
-        token = base64.b64decode(secret["root_token"]).decode('utf-8')
-        vault_client = hvac.Client(
-            url='http://vault-platform.platform.svc.cluster.local:8200', token=token)
+            secret = v1.read_namespaced_secret("vault-keys", "platform").data
+            token = base64.b64decode(secret["root_token"]).decode('utf-8')
+            vault_client = hvac.Client(
+                url='http://vault-platform.platform.svc.cluster.local:8200', token=token)
 
-        if eventName == "Deleted":
-            vault_client.sys.delete_policy(name=(policy_name or name))
-        else:
-            policy_hcl = context['object']['spec']['policy-hcl']
-            vault_client.sys.create_or_update_policy(
-                name=(policy_name or name), policy=policy_hcl)
+            if eventName == "Deleted":
+                vault_client.sys.delete_policy(name=(policy_name or name))
+            else:
+                policy_hcl = val['object']['spec']['policy-hcl']
+                vault_client.sys.create_or_update_policy(
+                    name=(policy_name or name), policy=policy_hcl)
     case _:
         print("Unknown hook data")
