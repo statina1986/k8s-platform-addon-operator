75e4710f2db8d18b9fef0da70c1f7e39cd3b6238
Alexander Muravyev <aleksander.muravyev@qvantel.com>
2023-01-11 13:19:53 +0200

    fix: better lazy loading of vault and k8s clients

--- common/python/k8s.py
+++ common/python/k8s.py
@@ -1,12 +1,14 @@
 from os import environ
 from kubernetes import client, config
 
-
+k8s = None
 
 def get_k8s_client():
-    if environ.get('INSIDE_CLUSTER', 'false') == 'true':
-        config.load_incluster_config()
-    else:
-        config.load_kube_config()
-    k8s = client.CoreV1Api()
+    global k8s
+    if k8s is None:
+        if environ.get('INSIDE_CLUSTER', 'false') == 'true':
+            config.load_incluster_config()
+        else:
+            config.load_kube_config()
+        k8s = client.CoreV1Api()
     return k8s
--- common/python/vault.py
+++ common/python/vault.py
@@ -3,8 +3,12 @@ import hvac
 from common.python.variables import *
 from common.python.k8s import *
 
+vault_client = None
+
 def get_vault_client():
-    secret = k8s.read_namespaced_secret(VAULT_SECRET_NAME, VAULT_SECRET_NAMESPACE).data
-    token = base64.b64decode(secret[VAULT_SECRET_ROOT_TOKEN]).decode('utf-8')
-    vault_client = hvac.Client(url=VAULT_ADDR, token=token)
+    global vault_client
+    if vault_client is None:    
+        secret = get_k8s_client().read_namespaced_secret(VAULT_SECRET_NAME, VAULT_SECRET_NAMESPACE).data
+        token = base64.b64decode(secret[VAULT_SECRET_ROOT_TOKEN]).decode('utf-8')
+        vault_client = hvac.Client(url=VAULT_ADDR, token=token)
     return vault_client
