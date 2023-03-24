403e1c56e063819823d1b1a2b9c12498af3dada1
Alexander Muravyev <aleksander.muravyev@qvantel.com>
2023-03-24 03:26:31 +0200

    feat: added k8s_store_secret_value inline function for hooks

--- common/python/inline.py
+++ common/python/inline.py
@@ -2,6 +2,8 @@ import secrets
 import boto3
 from common.python.vault import *
 from common.python.k8s import *
+from kubernetes.client.rest import ApiException
+
 
 def get_random_string(length):
     return secrets.token_urlsafe(length)
@@ -12,11 +14,31 @@ def aws_get_secret_value(secretId):
     response = client.get_secret_value(SecretId=secretId)
     return response
 
+
 def k8s_get_secret_value(secretName, secretNamespace, key):
     k8s = get_k8s_client()
     secret = k8s.read_namespaced_secret(secretName, secretNamespace).data
     value = base64.b64decode(secret[key]).decode('utf-8')
     return value
+
+
+def k8s_store_secret_value(secretName, secretNamespace, key, value):
+    k8s = get_k8s_client()
+    try:
+        secret = client.V1Secret(metadata=client.V1ObjectMeta(name=secretName))
+        k8s.create_namespaced_secret(namespace=secretNamespace, body=secret)
+    except client.rest.ApiException as e:
+        if e.status == 409:
+            logger.info("Secret already exist")
+        else:
+            raise e
+    patch = {
+        "data": {}
+    }
+    patch['data'][key] = base64.b64encode(value.encode('utf-8')).decode("utf-8")
+    k8s.patch_namespaced_secret(secretName, secretNamespace, patch)
+    return value
+
 
 def vault_store_secret(path, key, value):
     vault_client = get_vault_client()
