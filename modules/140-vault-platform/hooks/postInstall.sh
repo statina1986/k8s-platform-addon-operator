0fd4ecc7c9266b189c2b62bc02a700083f16b7ac
Sami J. Mäkinen <sjm@iki.fi>
2023-05-09 10:19:59 +0300

    KPLAT-101

--- modules/140-vault-platform/hooks/postInstall.sh
+++ modules/140-vault-platform/hooks/postInstall.sh
@@ -40,6 +40,18 @@ hook::trigger() {
   else
     qlog "Database secrets already enabled"
   fi
+
+  qlog "Checking audit logging"
+  curl::execute "--header 'X-Vault-Token: $token' '$VAULT_ADDR/v1/sys/audit'" 200
+  if [ ! "$(jq 'has("file/")' $CURL_RESULT)" == "true" ]
+  then
+    kubectl exec -n platform vault-platform-0 -- /bin/sh -c "vault login -no-print $token && \
+      vault audit enable file file_path=stdout"
+    qlog "Audit logging enabled"
+  else
+    qlog "Audit logging already enabled"
+  fi
 }
 
 common::run_hook "$@"
+
