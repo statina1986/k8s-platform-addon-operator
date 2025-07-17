e712108aca3a6f32b9481267343f89305140e596
Alexander Muravyev <aleksander.muravyev@qvantel.com>
2025-07-17 16:22:26 +0300

    other: removal of platform-tools-images as it was moved to https://stash.qvantel.net/projects/CP/repos/platform-k8s-tools

--- platform-tools-images/local-build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-timestamp=( `tar -c . | sha1sum` )
-
-docker build -t "local/$1:$timestamp" . --file "./$1.Dockerfile"
-docker tag "local/$1:$timestamp" "artifactory.qvantel.net/$1:$timestamp"
-docker push "artifactory.qvantel.net/$1:$timestamp"
