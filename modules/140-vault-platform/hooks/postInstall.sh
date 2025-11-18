db43bda86ca8e72b1b6d6a0c5c8b088053ca2d65
Alexander Muravyev <aleksander.muravyev@qvantel.com>
2025-11-18 13:51:37 +0200

    feat: adding 'secret-kv2' secret engine to Vault for applications secrets in KV2 format

--- modules/140-vault-platform/hooks/postInstall.sh
+++ modules/140-vault-platform/hooks/postInstall.sh
@@ -32,13 +32,22 @@ hook::trigger() {
     qlog "KV Secrets already enabled"
   fi
 
-  qlog "Ensuring kv2 secrets engine enabled"
+  qlog "Ensuring kv2 generic secrets engine enabled"
+  curl::execute "--header 'X-Vault-Token: $token' '$VAULT_ADDR/v1/sys/mounts'" 200
+  if [ ! "$(jq 'has("secret-v2/")' $CURL_RESULT)" == "true" ]; then
+    kubectl exec -n $ADDON_OPERATOR_NAMESPACE $VAULT_RELEASE_NAME-0 -- /bin/sh -c "vault login -no-print $token && \
+        vault secrets enable -path='secret-v2' kv-v2"
+  else
+    qlog "KV2 Generic Secrets already enabled"
+  fi
+
+  qlog "Ensuring kv2 platform secrets engine enabled"
   curl::execute "--header 'X-Vault-Token: $token' '$VAULT_ADDR/v1/sys/mounts'" 200
   if [ ! "$(jq 'has("platform-secret/")' $CURL_RESULT)" == "true" ]; then
     kubectl exec -n $ADDON_OPERATOR_NAMESPACE $VAULT_RELEASE_NAME-0 -- /bin/sh -c "vault login -no-print $token && \
         vault secrets enable -path='platform-secret' kv-v2"
   else
-    qlog "KV2 Secrets already enabled"
+    qlog "KV2 Platform Secrets already enabled"
   fi
 
   qlog "Ensuring database secrets engine enabled"
