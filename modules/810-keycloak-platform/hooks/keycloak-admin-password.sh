e00cf86d4ff45edce818041025d1eeff537eac10
Viljo Viitanen <viljo.viitanen@qvantel.com>
2026-01-15 15:37:27 +0200

    config: add enabled flag to limited admin as it requires 2026.04 QRP or newer
    issue: CUST-23989

--- modules/810-keycloak-platform/hooks/keycloak-admin-password.sh
+++ modules/810-keycloak-platform/hooks/keycloak-admin-password.sh
@@ -15,11 +15,20 @@ hook::trigger() {
     qlog "Vault is not enabled, skipping admin credentials hook"
     exit 0
   fi
-  
-  qlog "Inserting limited keycloak admin username and password to vault for system-spec qinstaller"
+ 
   token="$(vault::get_vault_token)"
-  password="$(kubectl::get_secret_opaque_kv keycloak-limited-admin-secret KEYCLOAK_LIMITED_ADMIN_PASSWORD $VAULT_SECRET_NAMESPACE)"
-  username="$(kubectl::get_secret_opaque_kv keycloak-limited-admin-secret KEYCLOAK_LIMITED_ADMIN $VAULT_SECRET_NAMESPACE)"
+ 
+  limitedAdminEnabled="$(common::get_values_value '.keycloakPlatform.limitedAdminEnabled')"
+  if [[ $limitedAdminEnabled == "true" ]] ; then
+    qlog "Inserting limited keycloak admin username and password to vault for system-spec qinstaller"
+    password="$(kubectl::get_secret_opaque_kv keycloak-limited-admin-secret KEYCLOAK_LIMITED_ADMIN_PASSWORD $VAULT_SECRET_NAMESPACE)"
+    username="$(kubectl::get_secret_opaque_kv keycloak-limited-admin-secret KEYCLOAK_LIMITED_ADMIN $VAULT_SECRET_NAMESPACE)"
+  else
+    qlog "Inserting full keycloak admin username and password to vault for system-spec qinstaller"
+    password="$(kubectl::get_secret_opaque_kv keycloak-admin-secret KEYCLOAK_ADMIN_PASSWORD $VAULT_SECRET_NAMESPACE)"
+    username="$(kubectl::get_secret_opaque_kv keycloak-admin-secret KEYCLOAK_ADMIN $VAULT_SECRET_NAMESPACE)"
+  fi
+
   OLD=`mktemp`
   ADD=`mktemp`
   NEW=`mktemp`
